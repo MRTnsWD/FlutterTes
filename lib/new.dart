1f22f1a6b04eb1257c9e4c2d3ce2828e39260f66
MRTnsWD <widiyantomartinus@gmail.com>
2022-08-11 22:21:49 +0700

    Martin

--- lib/new.dart
+++ lib/new.dart
@@ -9,6 +9,33 @@ class Res extends StatefulWidget {
 }
 
 class _ResState extends State<Res> {
+  int angka = 1;
+  var Date = DateFormat('dd/MM/yyyy').format(DateTime.now()),
+      Time = DateFormat('hh:mm:ss').format(DateTime.now());
+
+  @override
+  void initState() {
+    _timeString = _formatDateTime(DateTime.now());
+    Timer.periodic(Duration(seconds: 1), (Timer t) => _getTime());
+    super.initState();
+  }
+
+  des() {
+    if (angka != 1) {
+      setState(() {
+        angka--;
+      });
+    }
+    print(angka);
+  }
+
+  inc() {
+    setState(() {
+      angka++;
+      print(angka);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,11 +52,28 @@ class _ResState extends State<Res> {
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       Text(
-                        DateFormat('dd/MM/yyyy').format(DateTime.now()), style: TextStyle(fontSize: 18),
+                        Date,
+                        style: TextStyle(fontSize: 18),
                       ),
                       Text(
-                        DateFormat('hh:mm:ss').format(DateTime.now()),
-                      )
+                        Time,
+                        style: TextStyle(
+                            fontSize: 10 + double.parse(angka.toString())),
+                      ),
+                      Column(children: [
+                        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            // ignore: deprecated_member_use
+                            children: [
+                              RaisedButton(
+                                onPressed: () => des(),
+                                child: const Icon(Icons.add),
+                              ),
+                              RaisedButton(
+                                onPressed: () => inc(),
+                                child: Icon(Icons.add),
+                              ),
+                            ])
+                      ])
                     ],
                   ),
                 ),
